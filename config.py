9d58cc3318a81432d7cccf08a89c48d91f46c2b6
ryan.thompson <ryan.thompson.55.ctr@us.af.mil>
2016-05-13 13:50:57 -0400

    minor changes to get chapter 5 working

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ OPENID_PROVIDERS = [
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_app/db/flask.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
