f472cab66921148696dd709c0304ac43feebbd97
ryan.thompson <ryan.thompson.55.ctr@us.af.mil>
2016-05-16 09:10:20 -0400

    Chapter 7 complete and working.

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired
 from flask.ext.wtf import Form
 from wtforms import StringField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length
+from flask_app.models import User
 
 
 class LoginForm(Form):
@@ -13,3 +14,18 @@ class LoginForm(Form):
 class EditForm(Form):
     nickname = StringField('nickname', validators=[DataRequired()])
     about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
+
+    def __init__(self, original_nickname, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+        self.original_nickname = original_nickname
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+        if self.nickname.data == self.original_nickname:
+            return True
+        user = User.query.filter_by(nickname=self.nickname.data).first()
+        if user != None:
+            self.nickname.errors.append('This nickname is already in use. Please choose another one.')
+            return False
+        return True
